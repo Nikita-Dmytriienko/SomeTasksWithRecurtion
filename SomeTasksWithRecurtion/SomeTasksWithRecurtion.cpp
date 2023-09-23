dc04ea99ed0af576fe7bf337cd422c10068359fd
Nikita Dmytriienko <nik.dmitrienko2000@gmail.com>
2023-09-23 13:17:49 +0300

    my version

--- SomeTasksWithRecurtion/SomeTasksWithRecurtion.cpp
+++ SomeTasksWithRecurtion/SomeTasksWithRecurtion.cpp
@@ -1,29 +1,55 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//
+//
+//void printNumbers(int n) {
+//    if (n == 1) {
+//        std::cout << n << " ";
+//        return;
+//    }
+//
+//    printNumbers(n - 1);
+//    std::cout << n << " ";
+//}
+//
+//int main() {
+//    int n;
+//    std::cout << "Введите натуральное число n: ";
+//    std::cin >> n;
+//
+//    if (n <= 0) {
+//        std::cout << "Число должно быть натуральным." << std::endl;
+//        return 1;
+//    }
+//
+//    std::cout << "Числа от 1 до " << n << ": ";
+//    printNumbers(n);
+//    std::cout << std::endl;
+//
+//	system("pause>>void");
+//	return 0;
+//}
+#include <iostream>
 
+void recurtion(int number)
+{
+	if (number == 1)
+	{
+		std::cout << number << " ";
+		return;
+	}
 
-void printNumbers(int n) {
-    if (n == 1) {
-        std::cout << n << " ";
-        return;
-    }
-
-    printNumbers(n - 1);
-    std::cout << n << " ";
+		recurtion(number-1);
+		std::cout << number << " ";
+	
 }
 
-int main() {
-    int n;
-    std::cout << "Введите натуральное число n: ";
-    std::cin >> n;
+int main()
+{
+	int n;
+	std::cout << "Enter Number:";
+	std::cin >> n;
 
-    if (n <= 0) {
-        std::cout << "Число должно быть натуральным." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Числа от 1 до " << n << ": ";
-    printNumbers(n);
-    std::cout << std::endl;
+	recurtion(n);
 
 	system("pause>>void");
 	return 0;
