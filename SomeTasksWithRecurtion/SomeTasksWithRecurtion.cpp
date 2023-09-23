d60bf1961377b84b56863dda3359032027041587
Nikita Dmytriienko <nik.dmitrienko2000@gmail.com>
2023-09-23 12:34:29 +0300

    Добавьте файлы проекта.

---
+++ SomeTasksWithRecurtion/SomeTasksWithRecurtion.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+
+int recurtion(int number);
+
+int main()
+{
+	std::cout << recurtion(6);
+
+
+	system("pause>>void");
+	return 0;
+}
+
+int recurtion(int number)
+{
+	int i = 1;
+	if (i == number)
+		return number;
+	else std::cout << i+recurtion(number) << std::endl;
+}
